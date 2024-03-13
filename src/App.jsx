67e11f9e4ad5c54363630dc488c95f79212b874e
ofek ashkenazi <ofeka18@gmail.com>
2024-03-13 16:12:17 +0200

    work on main page added img to left side and content to right side.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import Header from "./components/Header"
+import merImg from "./assets/images/planet-mercury.svg"
+import extarnalLink from "./assets/images/icon-source.svg"
 
 export default function App() {
   const [plantState, setPlantState] = useState(0)
@@ -9,17 +11,57 @@ export default function App() {
   }
 
   useEffect(() => {
-    console.log(plantState)
   }, [plantState])
 
   return (
     <main className="main-layout-big-img-hero">
 
-      <Header handlePlantState={handlePlantState}/>
+      <Header handlePlantState={handlePlantState} />
 
       <section className="main-layout">
 
-        hello world
+        <div className="main-content-left-side">
+
+          <img src={merImg} alt="" />
+
+        </div>
+
+        <div className="main-content-right-side">
+
+          <h1>MERCURY</h1>
+
+          <p>Mercury is the smallest planet in the Solar
+            System and the closest to the Sun.
+            Its orbit around the Sun takes 87.97 Earth days,
+            the shortest of all the Sun's planets.
+            Mercury is one of four terrestrial planets
+            in the Solar System, and is a rocky body
+            like Earth.
+          </p>
+
+          <div className="flex wiki"><p>Source:  Wikipedia</p> <img src={extarnalLink} alt="" /></div>
+
+          <section className="right-side-down-container">
+
+            <div className="right-down-item">
+              <p>01</p>
+              <p>OVERVIEW</p>
+            </div>
+            <div className="right-down-item">
+              <p>02</p>
+              <p>Internal Structure</p>
+            </div>
+            <div className="right-down-item">
+              <p>03</p>
+              <p>Surface Geology</p>
+            </div>
+
+          </section>
+
+
+        </div>
+
+
       </section>
 
     </main>
