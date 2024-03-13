4b297e852cdfc9e5b2bbeb03efb34895e6ed4ffa
ofek ashkenazi <ofeka18@gmail.com>
2024-03-13 14:41:56 +0200

    fix font and work on some basic css from figama

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 import Header from "./components/Header"
 import background from "./assets/images/background-stars.svg"
 
-
-
 export default function app() {
-
-  function handleClick() {
-    alert("hi")
-  }
 
   return (
     <main className="main-layout-big-img-hero">
 
-      {/* <img src={background} alt="" /> */}
-
       <Header />
 
+      <section className="main-layout">
 
-      <section className="main-layout spartan-reg">
         hello world
       </section>
-      <button onClick={handleClick}>hello world</button>
+
     </main>
+
   )
-}
+} 
+ 
